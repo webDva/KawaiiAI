ee49098ee8593cd5c1929dde7340cfe3219fecdd
Web D.va <webNoDva@gmail.com>
2018-01-26 13:55:19 -0500

    replaced the tokenizer

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const Lexed = require("lexed").Lexed;
+const natural = require("natural");
 
 const categories = [ // a list of classes of documents
     {
         "class": "hungry", // a class of documents
         "documents": // a list of documents
             [
-                "im hungry", // a single document
-                "i want to eat",
-                "feed me baka",
-                "whats for dinner"
+                "i'm hungry!", // a single document
+                "i want to eat!",
+                "feed me, baka!",
+                "what's for dinner?"
             ]
     },
     {
         "class": "sleepy",
         "documents":
             [
-                "im sleepy",
-                "im tired",
+                "i'm sleepy",
+                "i'm tired",
                 "i must rest"
             ]
     },
@@ -24,8 +24,8 @@ const categories = [ // a list of classes of documents
         "class": "kawaii",
         "documents":
             [
-                "im kawaii",
-                "baka",
+                "i'm kawaii",
+                "baka!",
                 "pantsu"
             ]
     }
@@ -39,10 +39,17 @@ let classes = [];
 // processing each document
 
 // first tokenize each word in the document
+let tokenizer = new natural.TreebankWordTokenizer();
 categories.forEach((category) => {
     category.documents.forEach((sentence) => {
-        let w = new Lexed(sentence).lexer().tokens; // a list of a list of words (yes, two lists)
+        let w = tokenizer.tokenize(sentence);
         // add each word to the list of words
-        w[0].forEach((i) => words.push(i));
+        w.forEach((i) => words.push(i));
+        // add each document to the list of documents
+        documents.push(sentence);
     });
+    // add each class to the list of classes. we know that there's no duplicates
+    classes.push(category.class);
 });
+
+// now stem and lower each word and remove duplicates
